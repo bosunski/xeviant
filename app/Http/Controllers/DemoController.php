4e8a8e6e5bce2a8857195b6cd466212f8385a3a6
Olatunbosun Egberinde <bosunski@gmail.com>
2019-11-24 23:37:58 +0100

    WIP

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -9,10 +9,6 @@ class DemoController extends Controller
 {
     public function index(Request $request)
     {
-        $p = yield (new Promise(function ($resolve, $reject) use ($request) {
-            $resolve(response()->json($request->all() + ["name" => "Foo"], 200));
-        }));
-
-        return $p;
+        return "Hello World";
     }
 }
